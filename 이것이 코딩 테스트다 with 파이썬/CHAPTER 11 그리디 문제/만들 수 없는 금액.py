824741d9d7703a15629516171dc4a89c144f6ac3
SeoYounSeok <syounseok94@gmail.com>
2020-09-15 10:18:57 +0900

    Update 만들 수 없는 금액.py

--- 이것이 코딩 테스트다 with 파이썬/CHAPTER 11 그리디 문제/만들 수 없는 금액.py
+++ 이것이 코딩 테스트다 with 파이썬/CHAPTER 11 그리디 문제/만들 수 없는 금액.py
@@ -6,3 +6,18 @@
 
 # N = 3, 3,5,7 일 때 양의 정수 금액 중 최솟 값은 1원입니다.
 
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+target = 1
+for x in data:
+    # 만들 수 없는 금액을 찾았을 때 반복 종료
+    # 굳이 모든 값을 찾아서 계산할 필요 없음.
+    if target < x:
+        break
+    target += x
+
+# 만들 수 없는 금액 출력
+print(target)
