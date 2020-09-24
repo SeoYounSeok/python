b4fd9de3779cd7ffda4df36afb0b5465c2c68cc7
SeoYounSeok <syounseok94@gmail.com>
2020-09-24 20:52:54 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스 코딩테스트/해시/완주하지 못한 선수.py
+++ 프로그래머스 코딩테스트/해시/완주하지 못한 선수.py
@@ -1,1 +1,16 @@
-# 완주하지 못한 선수 제작중 ㄱㄷ
+# 완주하지 못한 선수
+
+def solution(participant, completion):
+    answer = ''
+
+    new_participant = sorted(participant)
+    new_completion = sorted(completion)
+
+    if new_participant[-1] != new_completion[-1]:
+        return new_participant[-1]
+
+    for i in range(len(new_completion)):
+        if new_participant[i] != new_completion[i]:
+            return new_participant[i]
+
+    return answer
