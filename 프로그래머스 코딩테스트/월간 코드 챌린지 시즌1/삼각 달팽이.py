cf4aec7faac51b9c4d2e28c5c89e5274860f118a
SeoYounSeok <syounseok94@gmail.com>
2020-10-09 15:27:17 +0900

    Update 삼각 달팽이.py

--- 프로그래머스 코딩테스트/월간 코드 챌린지 시즌1/삼각 달팽이.py
+++ 프로그래머스 코딩테스트/월간 코드 챌린지 시즌1/삼각 달팽이.py
@@ -5,3 +5,30 @@
 # 각 행에 있는 원소들을 차례대로 정답 배열에 담아 return 하도록 하면 됩니다.
 
 # 풀이를 시작하기 전 달팽이 배열을 먼저 공부하고 오겠습니다.
+
+def solution(n):
+    answer = []
+    arr = [[0 for _ in range(n)] for _ in range(n)]
+    
+    x = -1 
+    y = 0
+    number = 1
+    
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:
+                x += 1
+            elif i % 3 == 1:
+                y += 1
+            elif i % 3 == 2:
+                x -= 1
+                y -= 1
+            arr[x][y] = number
+            number += 1
+   
+    for i in range(n):
+        for j in range(0, i+1):
+            if arr[i][j] != 0:
+                answer.append(arr[i][j])
+
+    return answer
